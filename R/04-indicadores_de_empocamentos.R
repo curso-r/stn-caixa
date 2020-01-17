9525db1faf0268757674acf74ec3bdeb1f099f2d
Athos Damiani <athos.damiani@gmail.com>
2020-01-17 16:19:28 -0300

    trelliscope da disponibilidade liquida (tentativa de colocar na pasta docs pra aparecer na net)

--- R/04-indicadores_de_empocamentos.R
+++ R/04-indicadores_de_empocamentos.R
@@ -1,3 +1,7 @@
+library(tidyverse)
+
+disponibilidades_liquidas_diarias <- read_rds("data/disponibilidades_liquidas_diarias.rds")
+
 # indicador de disponibilidade liquida ---------------------------
 indicadores <- disponibilidades_liquidas_diarias %>%
   group_by(
@@ -54,7 +58,73 @@ indicadores  %>%
   ggplot(aes(x = integral_sobre_media_dos_gastos_log, y = NO_FONTE_RECURSO, colour = NO_FONTE_RECURSO)) +
   geom_jitter(aes(size = log_neg(integral))) +
   geom_hline(yintercept = 0, colour = "red") +
-  labs(title = orgaos_sorteados$NO_ORGAO, subtitle = orgaos_sorteados$NO_FONTE_RECURSO) +
+  # labs(title = orgaos_sorteados$NO_ORGAO, subtitle = orgaos_sorteados$NO_FONTE_RECURSO) +
   geom_vline(xintercept = 0, colour = "red")
   
-  
+###
+fonte = "RECURSOS FINANCEIROS DIRETAMENTE ARRECADADOS"
+orgao = "MINISTERIO DA JUSTICA E SEGURANCA PUBLICA"
+
+disponibilidades_liquidas_diarias %>%
+  filter(
+    NO_FONTE_RECURSO %in% fonte,
+    NO_ORGAO %in% orgao
+  ) %>%
+  ggplot(aes(x = NO_DIA_COMPLETO_dmy, y = disponibilidade_liquida)) +
+  geom_line() +
+  facet_wrap(~NO_UG, scale = "free_y")
+
+
+###
+fonte = "RECURSOS FINANCEIROS DIRETAMENTE ARRECADADOS"
+orgao = "MINISTERIO DA JUSTICA E SEGURANCA PUBLICA"
+
+disponibilidades_liquidas_diarias %>%
+  filter(
+    NO_FONTE_RECURSO %in% fonte,
+    NO_ORGAO %in% orgao
+  ) %>%
+  ggplot(aes(x = NO_DIA_COMPLETO_dmy, y = disponibilidade_liquida, colour = NO_UG)) +
+  geom_line() +
+  geom_vline(data = trinta_e_uns_de_dezembro, aes(xintercept = NO_DIA_COMPLETO_dmy), colour = "purple", linetype = "dashed", size = 0.2) +
+  facet_wrap(~NO_UG, scale = "free_y")
+
+
+library(trelliscopejs)
+
+trinta_e_uns_de_dezembro <- tibble(
+  NO_DIA_COMPLETO_dmy = as.Date(c("2017-12-31", "2018-12-31", "2019-12-31"))
+)
+
+### Um gráfico por fonte. Cada curva é uma UG
+indicadores %>% 
+  ungroup %>%
+  arrange(desc(disponibilidade_estritamente_crescente)) %>%
+  distinct(NO_FONTE_RECURSO, NO_ORGAO) %>%
+  head(1) %>%
+  right_join(
+    disponibilidades_liquidas_diarias
+  ) %>%
+  ggplot(aes(x = NO_DIA_COMPLETO_dmy, y = disponibilidade_liquida, colour = NO_UG)) +
+  geom_line(show.legend = FALSE) +
+  geom_vline(data = trinta_e_uns_de_dezembro, aes(xintercept = NO_DIA_COMPLETO_dmy), colour = "purple", linetype = "dashed", size = 0.2) +
+  facet_trelliscope(~ NO_FONTE_RECURSO + NO_ORGAO, scale = "free_y", nrow = 1, ncol = 3)
+
+
+
+
+### Um gráfico por fonte e UG
+indicadores %>% 
+  ungroup %>%
+  arrange(desc(disponibilidade_estritamente_crescente)) %>%
+  distinct(NO_FONTE_RECURSO, NO_ORGAO) %>%
+  head(1) %>%
+  right_join(
+    disponibilidades_liquidas_diarias
+  ) %>%
+  group_by(NO_FONTE_RECURSO, NO_UG, NO_ORGAO) %>%
+  filter(n() > 10) %>%
+  ggplot(aes(x = NO_DIA_COMPLETO_dmy, y = disponibilidade_liquida, colour = NO_UG)) +
+  geom_line(show.legend = FALSE) +
+  geom_vline(data = trinta_e_uns_de_dezembro, aes(xintercept = NO_DIA_COMPLETO_dmy), colour = "purple", linetype = "dashed", size = 0.2) +
+  facet_trelliscope(~ NO_FONTE_RECURSO + NO_UG + NO_ORGAO, scale = "free_y", nrow = 2, ncol = 3, path = "html")
